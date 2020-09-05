db8c3fda17eeae518006fe265a90107d99913699
Margareth_Vela <vel19458@uvg.edu.gt>
2020-09-05 15:32:39 -0600

    Ejercicio 6

--- Ejercicio_6/MSF1.v
+++ Ejercicio_6/MSF1.v
@@ -1,20 +1,44 @@
-module ejercicio1(input wire clk, reset, a, b, output reg Y);
+module ff_D(input wire clock,reset,input wire d,output reg Y); //Flip flop tipo d
+  always @ (posedge clock, posedge reset) begin
+    if(reset) begin
+      Y <= 1'b0;
+    end
+    else
+      Y <= d;
+    end
+endmodule
 
-  typedef enum reg {S0, S1} statetype;
-  statetype state.nextstate;
+module FSM_Mealy(input wire A, B,clock, reset,output wire S0, S1, S0F, S1F, Y); //Ejercicio 1
 
-  always @(posedge clk, posedge reset) begin
-    if (reset) state <= S0;
-    else state <= nextstate;
-  end
+    wire S11, S00; //Variables internas
 
-  always @(a or b) begin
-    case (state)
-      S0: if(a) nextstate = S0;
-      else nextstate = S1;
-      S1: if(a) nextstate = S0;
-      else nextstate = S1;
-      default: nextstate = S0;
-    endcase
+    assign S0F = (~S11 & ~S00 & A);   //Determinar valores para los estados
+    assign S1F = ((S00 & B) | (S11 & A & B));
 
-  assign Y = ()
+    ff_D G1(clock, reset,S1F, S11); //Flip flop tipo d para obtener los siguientes estados
+    ff_D G2(clock, reset,S0F, S00);
+    assign S0 = S00;
+    assign S1 = S11;
+    assign Y = (S11 & A & B); //Salida
+
+endmodule
+
+module FSM_Moore(input wire A, input wire clock, reset, output wire S0, S1, S2, S0F, S1F, S2F, Y1, Y2, Y3); //Ejercicio 3
+    wire S00, S11, S22;//Variables internas
+
+    assign S0F = ~S00; //Determinar valores  para estados
+    assign S1F = (~S11 & ~S00 & ~A) | (S11 & S0 & ~A) | (S11 & ~S00 & A) | (~S11 & S00 & A);
+    assign S2F = (S22 & S11 & ~A) | (S22 & ~S00 & A) | (S22 & ~S11 & ~S00) | (~S22 & S11 & S00 & A);
+
+    ff_D G1(clock, reset, S0F, S00);//Flip flop tipo d para obtener los siguientes estados
+    ff_D G2(clock, reset, S1F, S11);
+    ff_D G3(clock, reset, S2F, S22);
+    assign S0 = S00;
+    assign S1 = S11;
+    assign S2 = S22;
+
+    assign Y3 = S22; //Salida de 3 bits
+    assign Y2 = (~S22 & S11) | (S22 & ~S11);
+    assign Y1 = (S11 & ~S00) | (~S11 & S00);
+
+endmodule
---
+++ Ejercicio_6/MSF1_tb.v
@@ -0,0 +1,64 @@
+module testbench();
+
+  reg clk,reset, clock, reset1;
+  reg A, B, A1;
+  wire S0, S1, S0F, S1F, Y, S00, S11, S22, S0F1, S1F1, S2F1, Y1, Y2, Y3;
+
+  always
+    begin
+      clk <= 1;
+      #1 clk <= ~clk;// se realiza el cambio del reloj
+      #1;
+  end
+
+  FSM_Mealy G1(A, B,clk,reset,S0,S1, S0F, S1F, Y);
+
+  always
+    begin
+      clock <= 1;
+      #1 clock <= ~clock;// se realiza el cambio del reloj
+      #1;
+  end
+
+ FSM_Moore G2(A1, clock, reset1, S00, S11, S22, S0F1, S1F1, S2F1, Y1, Y2, Y3);// Ejercicio 3
+
+  initial begin //Ejercicio 1
+    $display("\n");
+    $display("Ejercicio 1 - Mealy");
+    $display(" S1 S0  | A B | S1F S0F | Y");
+    $display(" --------------------------- ");
+    $monitor(" %b %b     %b  %b     %b  %b    %b ", S1, S0, A, B, S1F, S0F, Y );
+       A = 0; B = 0;
+    #1 A = 1; B = 0;
+    #1 A = 1; B = 1;
+    #1 A = 1; B = 1;
+    #1 A = 1; B = 1;
+    #1 A = 0; B = 0;
+    #1 A = 1; B = 0;
+    #1 A = 0; B = 0;
+  end
+
+  initial begin
+       #15
+       $display("\n");
+       $display(" Ejercicio 3 - Escala de gray ");
+       $display(" S2 S1 S0 | A | S2F S1F S0F | Y3 Y2 Y1");
+       $display(" ---------------------------");
+       $monitor(" %b %b %b    %b   %b  %b  %b   %b %b %b ", S22, S11, S00, A1, S2F1, S1F1, S0F1, Y3, Y2, Y1);
+
+       A1 = 1;
+      #1 reset1 = 1;
+      #1 reset1 = 0;
+      #8 A1 = 0;
+      #8 A1 = 1;
+     end
+
+     initial
+      #35 $finish;
+
+  initial begin
+        $dumpfile("MSF1_tb.vcd");
+        $dumpvars(0, testbench);
+      end
+
+endmodule
